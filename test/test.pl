3fcdc7f76f30d678ff71d83bf529015dd76c04de
Nicolás Andrés Gallinal <ngallinal@hexacta.com>
2019-05-12 17:42:51 -0300

    Restore ability to load tests from the test folder

--- test/test.pl
+++ test/test.pl
@@ -1,4 +1,12 @@
-:- use_module(prolog/tokenize).
+:- dynamic user:file_search_path/2.
+:- multifile user:file_search_path/2.
+
+% Add the package source files relative to the current file location
+:- prolog_load_context(directory, Dir),
+   atom_concat(Dir, '/../prolog', PackageDir),
+   asserta(user:file_search_path(package, PackageDir)).
+
+:- use_module(package(tokenize)).
 :- begin_tests(tokenize).
 
 test('Hello, Tokenize!',
