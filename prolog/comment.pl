2abd5a1068e4e9df76eb6bd99b1ce753197432f9
Stefan Israelsson Tampe <stefan.itampe@gmail.com>
2019-05-15 20:06:59 +0200

    * CLeanup * Added comments * reworked copy_term removed it * strict matching - demand end tags * removed line comments use them higher up * proper tests?

--- prolog/comment.pl
+++ prolog/comment.pl
@@ -2,15 +2,36 @@
 module(tokenize(comment)
        [comment/2,
         comment_rec/2,
-        comment_token/2,
-        comment_token_rec/2]).
+        comment_token/3,
+        comment_token_rec/3]).
 */
+%% Author: Stefan Israelsson Tampe (stefan.itampe@gmail.com)
+%% Interface
+%% Start and End matchers is either a proper matcher (-->) or an atom to be
+%% matched as start or end match of the comment.
+%%
+%% comment(StartMatcher,EndMatcher)
+%%   - anonymously match a non recursive comment
+%%
+%% comment_rec(StartMatcher,EndMatcher)
+%%   - anonymously march a recursive comment
+%%
+%% coment_token(StartMatcher,EndMatcher,MatchedSequence)
+%%   - match an unrecursive comment outputs the matched sequence used
+%%     for building a resulting comment token
+%%
+%% coment_token_rec(StartMatcher,EndMatcher,MatchedSequence)
+%%   - match an recursive comment outputs the matched sequence used
+%%     for building a resulting comment token
 
-dcgtrue(U,U).
-
+% will be used as id(Codes) as match Codes under (-->) and used as such in non
+% tokenizing comments. Else the extra argument id(Codes,Match) will be
+% used as matcher (-->) in tokzing matching, Match==Codes is the matched
+% sequence
 id(X) --> X.
 id(X,X) --> X.
 
+% translate a matcher to a macther or an atom to a matcher that matches the atom
 tr(S,SS) :-
     atom(S) ->
         (
@@ -19,26 +40,19 @@ tr(S,SS) :-
         );
     SS=S.
 
-eol     --> {atom_codes('\n',E)},id(E).
-eol(HS) --> {atom_codes('\n',E)},id(E,HS).
- 
+%% comment/2 construction - non recursive non tokenizing matcher
 comment_body(E) --> call(E),!.
 comment_body(E) --> [_],comment_body(E).
-comment_body(_) --> [].
                            
 comment(S,E) -->
     {
-        copy_term([S,E],[S2,E2]),
-        tr(S2,SS),
-        tr(E2,EE)
+        tr(S,SS),
+        tr(E,EE)
     },
     call(SS),
     comment_body(EE).
 
-line_comment(S) -->
-    {tr(S,SS)},
-    comment_body(SS,eol).
-
+%% comment_token/3 construction - non recursive tokenizing matcher
 comment_body_token(E,Text) -->
     call(E,HE),!,
     {append(HE,[],Text)}.
@@ -46,23 +60,20 @@ comment_body_token(E,Text) -->
 comment_body_token(E,[X|L]) -->
     [X],
     comment_body_token(E,L).
-
-comment_body_token(_,[]) --> [].
                            
 comment_token(S,E,Text) -->
     {
-        copy_term([S,E],[S2,E2]),
-        tr(S2,SS),
-        tr(E2,EE)
+        tr(S,SS),
+        tr(E,EE)
     },
     call(SS,HS),
     {append(HS,T,Text)},
     comment_body_token(EE,T).
 
-line_comment_token(S,Text) -->
-    {tr(S,SS)},
-    comment_body_token(SS,eol,Text).
+%% comment_token_rec/3 construction - recursive tokenizing matcher
 
+% Use this as the initial continuation, will just tidy up the mathed resul
+% with ending the list with [].
 comment_body_rec_start(_,_,[]).
 
 comment_body_token_rec(_,E,Cont,Text) -->
@@ -79,18 +90,16 @@ comment_body_token_rec(S,E,Cont,[X|L]) -->
     [X],
     comment_body_token_rec(S,E,Cont,L).
 
-comment_body_token_rec(_,_,_,_,_,[]) --> dcgtrue.
-
 comment_token_rec(S,E,Text) -->
     {
-        copy_term([S,E],[S2,E2]),
-        tr(S2,SS),
-        tr(E2,EE)
+        tr(S,SS),
+        tr(E,EE)
     },
     call(SS,HS),
     {append(HS,T,Text)},
     comment_body_token_rec(SS,EE,comment_body_rec_start,T).
 
+%% comment_rec/3 construction - recursive non tokenizing matcher
 comment_body_rec(_,E) -->
     call(E),!.
 
@@ -103,8 +112,6 @@ comment_body_rec(S,E) -->
     [_],
     comment_body_rec(S,E).
 
-comment_body_rec(_,_) --> [].
-
 comment_rec(S,E) -->
     {
         tr(S,SS),
@@ -113,6 +120,8 @@ comment_rec(S,E) -->
     call(SS),
     comment_body_rec(SS,EE).
 
+
+%% ============================ testcode ==============================
 test(Tok,S,U) :-
     atom_codes(S,SS),
     call_dcg(Tok,SS,U).
@@ -131,34 +140,79 @@ test_comment_token_rec(S,T) :-
     test(comment_token_rec('<','>',TT),S,[]),
     atom_codes(T,TT).
 
-tester([]).
-tester([X|L]) :-
-    write_term(test(X),[]),
+start(AA) :-
     (
-        call(X) -> write(' ... OK') ; write(' ... FAIL')
+        catch(b_getval(a,[N,A]),_,N=0) ->
+          true;
+        N=0
     ),
-    nl,
-    tester(L).
+    NN is N + 1,
+    (
+        N == 0 ->
+          AA = _;
+        AA = A
+    ),
+    b_setval(a,[NN,AA]).
 
-a(A,B)   --> {atom_codes(A,AA)},AA,[B].
-a(A,B,C) --> {atom_codes(A,AA)},AA,[B],{append(AA,[B],C)}.
-b(A,B)   --> {atom_codes(B,BB)},[A],BB.
-b(A,B,C) --> {atom_codes(B,BB)},[A],BB,{append([A],BB,C)}.
+end(A) :-
+    b_getval(a,[N,A]),
+    NN is N - 1,
+    b_setval(a,[NN,A]).
+    
+a(A) --> 
+  {atom_codes(A,AA)},
+   AA,
+   {start(B)},
+   [B].
+
+a(A,C) --> 
+  {atom_codes(A,AA)},
+   AA,
+   {start(B)},
+   [B],
+   {append(AA,[B],C)}.
+
+b(A) -->
+ {end(B)},
+ [B],
+ {atom_codes(A,AA)},
+  AA.
+
+b(A,C) -->
+ {end(B)},
+ [B],
+ {atom_codes(A,AA)},
+  AA,
+  {append([B],AA,C)}.
 
 test_adapt(S,T) :-
-    test(comment_token_rec(a('<',U),b(U,'>'),TT),S,[]),
+    test(comment_token_rec(a('<'),b('>'),TT),S,[]),
     atom_codes(T,TT).
     
 
-/*
-tester(
-    [test_comment('<alla>'),
-     test_comment_rec('<alla<balla>>'),
-     test_comment_token('<alla>','<alla>'),
-     test_comment_token_rec('<alla<balla>>','<alla<balla>>'),
-     test_adapt('<1 alla> <1 balla> 1>1><2 alla> <2 alla> 2>2>',
-                '<1 alla> <1 balla> 1>1><2 alla> <2 alla> 2>2>')]).
-*/
+:- multifile test/2.
+             
+test('Test comment',[true(test_comment('<alla>'))]) :- true.
+test('Test comment_rec',[true(test_comment_rec('<alla<balla>>'))]) :- true.
+test('Test comment_token',
+     [true(A==B)]) :- 
+     A='<alla>', 
+     test_comment_token(A,B).
+
+test('Test comment_token_rec',
+     [true(A==B)]) :- 
+   A='<alla<balla>>', 
+   test_comment_token(A,B).
+
+test('Test comment_token_rec advanced 1',
+     [true(A==B)]) :- 
+   A='<1 alla2> <1 balla2> 1>1>', 
+   test_adapt(A,B).
+
+test('Test comment_token_rec advanced 2',
+     [true(A==B)]) :- 
+   A='<2 alla1> <2 balla1> 2>2>', 
+   test_adapt(A,B).
 
     
         
